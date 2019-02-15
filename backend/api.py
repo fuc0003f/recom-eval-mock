05cad293e8ead2c0a75fe3c994c530bed4f8af3f
Florian Fuchs <Florian.Fuchs@P7S1Applications.de>
2019-02-15 15:45:59 +0100

    backend folder

---
+++ backend/api.py
@@ -0,0 +1,48 @@
+"""
+Simple API to return similar brands.
+This is just a prototype, so no worries about the details of the file.
+"""
+
+import json
+from typing import List
+from flask import Flask, jsonify
+
+# define some functions 
+def get_similar_brands(brand_id: int) -> List[int]:
+    """Run a `complicated` ML model to suggest brands"""
+    return mocked_brand_suggestions[str(brand_id)]
+
+def get_all_brands():
+    """return all brand ids inside the brands.json"""
+    brand_id_list = []
+    for brand_id in mocked_brand_suggestions:
+        brand_id_list.append(int(brand_id))
+    return brand_id_list
+
+app = Flask(__name__)
+
+# define some api endpoints
+@app.route('/brands/<int:brand_id>/similar')
+def similar_brands(brand_id: int):
+    """Return similar brands."""
+    try:
+        return jsonify(get_similar_brands(brand_id))
+    except KeyError:
+        return jsonify(error=404,
+                       message="The brand is not found in the test index")
+
+@app.route('/brands')
+def all_brands():
+    """Return all available brands."""
+    try:
+        return jsonify(get_all_brands())
+    except KeyError:
+        return jsonify(error=404,
+                       message="No contents are found")
+
+if __name__ == '__main__':
+    with open('brands.json') as jf:
+        mocked_brand_suggestions = json.load(jf)
+
+
+    app.run(host='0.0.0.0')
---
+++ backend/task.md
@@ -0,0 +1,27 @@
+# Feature user feedback
+
+At **Stylight** we build features for our customers. In many cases, before rolling out the features directly to the users, we would like to get feedback as soon as possible.
+
+## Idea
+Imagine, that we are testing a feature, that allows us recommending brands. Given brand `A` the system suggesting  a `list of brand IDs` ordered by relevancy.
+
+## System
+The task of this small challenge is to build a prototype of the system, that would allow us to query API of such brand recommender system and let users rate the quality of the recommendations.
+
+At the end of the day, our data science and product people would like to analyze the rates, that users gave for particular brand recommendations.
+So we can iterate on the version and get the best users experience.
+
+
+## Deliverable
+- please send back a link to Github repository or a zip file with code
+- provide a way how the code would be executed on our side
+- all ideas how the system should be improved you can write to the readme file
+
+
+## Setup
+There is a simple API which you'll need, to install it simply use pip. To run the API, simply run the api.py file.
+
+```
+pip install -r requirements.txt
+python api.py
+```
