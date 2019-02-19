984ec57eda2ed7b578eb811cc497099a72fe1210
Florian Fuchs <Florian.Fuchs@P7S1Applications.de>
2019-02-19 15:53:34 +0100

    api changes for brand details

--- backend/api.py
+++ backend/api.py
@@ -13,7 +13,11 @@ def get_similar_brands(brand_id: int) -> List[int]:
     """Run a `complicated` ML model to suggest brands"""
     return mocked_brand_suggestions[str(brand_id)]
 
-def get_all_brand_ids():
+def get_brand_info(brand_id: int):
+    """return the information of a specific brand"""
+    return mocked_brand_infos[str(brand_id)]
+
+def get_all_brand_ids() -> List[int]:
     """return all brand ids inside the brands.json"""
     brand_id_list = []
     for brand_id in mocked_brand_suggestions:
@@ -46,9 +50,20 @@ def all_brands():
         return jsonify(error=404,
                        message="No contents are found")
 
+@app.route('/brandinfo/<int:brand_id>')
+def brand_info(brand_id: int):
+    """Return the infos of a brand."""
+    try:
+        return jsonify(get_brand_info(brand_id))
+    except KeyError:
+        return jsonify(error=404,
+                       message="No info available for brand with id " + brand_id)
+
 if __name__ == '__main__':
     with open('brands.json') as jf:
         mocked_brand_suggestions = json.load(jf)
+    with open('brand-info.json') as jf:
+        mocked_brand_infos = json.load(jf)
 
 
     app.run(host='0.0.0.0')
