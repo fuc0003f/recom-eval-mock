e1188a917ea98ff5c3586ec425f0a91d1673d802
Florian Fuchs <Florian.Fuchs@P7S1Applications.de>
2019-02-19 08:57:18 +0100

    api request returns all the brand data now

--- backend/api.py
+++ backend/api.py
@@ -6,20 +6,26 @@ This is just a prototype, so no worries about the details of the file.
 import json
 from typing import List
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 # define some functions 
 def get_similar_brands(brand_id: int) -> List[int]:
     """Run a `complicated` ML model to suggest brands"""
     return mocked_brand_suggestions[str(brand_id)]
 
-def get_all_brands():
+def get_all_brand_ids():
     """return all brand ids inside the brands.json"""
     brand_id_list = []
     for brand_id in mocked_brand_suggestions:
         brand_id_list.append(int(brand_id))
     return brand_id_list
 
+def get_all_brands():
+    """return all brand data"""
+    return mocked_brand_suggestions
+
 app = Flask(__name__)
+CORS(app)
 
 # define some api endpoints
 @app.route('/brands/<int:brand_id>/similar')
